a612158bcf8de1207e9749576f5f1468e89b5b87
Shreyas-hack245 <19776shreyasbhat@gmail.com>
2026-05-18 23:04:10 +0530

    add Elasticsearch connector mod to perform log searches

--- backend/app/siem_connector.py
+++ backend/app/siem_connector.py
@@ -1,9 +1,19 @@
 from elasticsearch import Elasticsearch
 
-def connect_elasticsearch():
+# Connect to Elasticsearch
+es = Elasticsearch("http://localhost:9200")
 
-    es = Elasticsearch(
-        "http://localhost:9200"
-    )
+def search_logs(index_name, query):
 
-    return es
+    try:
+        response = es.search(
+            index=index_name,
+            body=query
+        )
+
+        return response
+
+    except Exception as e:
+        return {
+            "error": str(e)
+        }
